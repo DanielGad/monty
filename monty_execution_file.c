ef175e83b0d202e974a181793dd5796267c4e514
Adeyemi Oluwagbenga Daniel <88782647+DanielGad@users.noreply.github.com>
2023-06-22 16:58:34 +0100

    Update monty_execution_file.c

--- monty_execution_file.c
+++ monty_execution_file.c
@@ -111,7 +111,6 @@ void (*get_op_func(char *opcode))(stack_t**, unsigned int)
 int run_monty(FILE *script_fd) {
     stack_t *stack = NULL;
     char *line = NULL;
-    size_t len = 0, exit_status = EXIT_SUCCESS;
     unsigned int line_number = 0, prev_tok_len = 0;
     void (*op_func)(stack_t**, unsigned int);
 
