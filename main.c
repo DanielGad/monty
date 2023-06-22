69736f23cbe1934e1921543bd79f3b71fc039356
Adeyemi Oluwagbenga Daniel <88782647+DanielGad@users.noreply.github.com>
2023-06-22 17:39:27 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "monty.h"
+
+#define MAX_LINE_LENGTH 1024
 
 bus_t bus = {NULL, NULL, NULL, 0};
 
 /**
+ * read_line - Custom line reading function
+ * @file: File pointer
+ * @line: Buffer to store the read line
+ * @size: Size of the buffer
+ * Return: Number of characters read, -1 on failure or end of file
+ */
+ssize_t read_line(FILE *file, char **line, size_t *size)
+{
+    if (!line || !size || !file)
+        return -1;
+
+    ssize_t read = 0;
+    size_t len = 0;
+    char *buffer = NULL;
+
+    if (getline(&buffer, &len, file) != -1)
+    {
+        read = strlen(buffer);
+        if (read > 0 && buffer[read - 1] == '\n')
+            buffer[read - 1] = '\0';
+
+        *line = malloc((read + 1) * sizeof(char));
+        if (*line)
+        {
+            strcpy(*line, buffer);
+            *size = read;
+        }
+        else
+        {
+            fprintf(stderr, "Error: Memory allocation failed\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    free(buffer);
+    return read;
+}
+
+/**
  * main - Monty code interpreter
- * @argc: number of arguments
- * @argv: array of arguments
+ * @argc: Number of arguments
+ * @argv: Array of arguments
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
@@ -23,21 +65,26 @@ int main(int argc, char *argv[])
         fprintf(stderr, "USAGE: monty file\n");
         exit(EXIT_FAILURE);
     }
+
     file = fopen(argv[1], "r");
     bus.file = file;
+
     if (!file)
     {
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
-    while (getline(&content, &size, file) != -1)
+
+    while (read_line(file, &content, &size) != -1)
     {
         bus.content = content;
         counter++;
         execute(content, &stack, counter, file);
+        free(content);
     }
-    free(content);
+
     free_stack(stack);
     fclose(file);
+
     return (0);
 }
